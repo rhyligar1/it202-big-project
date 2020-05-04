0948be7f402fd06ee0b06c0f2da8321fda515b10
rhyligar1 <59834604+rhyligar1@users.noreply.github.com>
2020-05-04 10:41:12 -0500

    Final Commit
    Everything is officially working and I even included a little favicon.

--- weather.js
+++ weather.js
@@ -1,27 +1,23 @@
-import {MDCMenu} from '@material/menu';
-
-const menu = new MDCMenu(document.querySelector('.mdc-menu'));
-menu.open = true;
-
 var button = document.querySelector('.button')
 var inputValue = document.querySelector('.inputValue')
 var name = document.querySelector('.name')
 var desc = document.querySelector('.desc')
 var temp = document.querySelector('.temp')
 
-button.addEventListener('click', function(){
-fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=a8647896d872d02af929705a13740e33')
-  .then(response => response.json())
-  .then(data => {
-    var nameValue = data['name'];
-    var tempValue = data['main']['temp'];
-    var descValue = data['weather'][0]['description'];
-
-    name.innerHTML = nameValue;
-    temp.innerHTML = tempValue;
-    desc.innerHTML = descValue;
-  })
+button.addEventListener('click', function() {
+  fetch('https://api.openweathermap.org/data/2.5/weather?q=' + inputValue.value + '&appid=a8647896d872d02af929705a13740e33')
+    .then(response => response.json())
+    .then(data => {
+      var nameValue = data['name'];
+      var tempValue = data['main']['temp'];
+      tempValue = ((tempValue-273.15)*9/5)+32;
+      var descValue = data['weather'][0]['description'];
 
 
-  .catch(err => alert("Incorrect City Name!"))
+      temp.innerHTML = "It feels like "+Math.round(tempValue)+ " °F right now.";
+      desc.innerHTML = "If you look at the sky, you should see "+descValue;
+    })
+
+
+    .catch(err => alert("Incorrect City Name!"))
 })
